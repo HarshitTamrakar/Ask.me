8750d3aebab352e939b8125c1b05e3ca72bb62b5
harshit <harshit@desktop.com>
2020-08-12 02:41:41 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,19 +1,105 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let db = require("./modules/database");
 
 let app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
+
 const port = 3000;
 
 app.get("/home", function(req, res){
-  res.render("pages/home");
+  db.getAllQuestions(function(err, questionList){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("pages/home", {
+        questionList: questionList
+      });
+    }
+  })
 });
 
 app.get("/", function(req, res){
   res.redirect("/home");
 });
+
+app.get("/add-question", function(req, res){
+  res.render("pages/add-question");
+});
+
+app.post("/add-question", function(req, res){
+  let questionBody = req.body;
+  let questionTitle = questionBody.questionTitle;
+  let questionDescription = questionBody.questionDescription;
+  let questionURL = questionBody.questionURL;
+  let question = {
+    title: questionTitle,
+    description: questionDescription,
+    imageURL: questionURL
+  };
+  db.insertQuestion(question);
+  res.redirect("/home");
+});
+
+app.get("/add-answer/:id", function(req, res){
+  let id = req.params.id;
+  db.getQuestion(id, function(err, question){
+    if(err){
+      console.log(err);
+    }else{
+      console.log(question);
+      res.render("pages/add-answer", {
+        question: question
+      })
+    }
+  })
+});
+
+app.post("/add-answer/", function(req, res){
+  let body = req.body;
+  let id = body.submit;
+  let answerText = body.answer;
+  let answerImageURL = body.answerImageURL;
+  if(answerImageURL == null){
+    answerImageURL = "";
+  }
+  db.insertAnswer(id, answerText, answerImageURL);
+  res.redirect("/view/" + id);
+});
+
+app.post("/upvote/:questionID/:answerID", function(req, res){
+  let questionId = req.params.questionID;
+  let answerId = req.params.answerID;
+  db.updateUpvotes(questionId, answerId);
+  res.redirect("/view/" + questionId);
+});
+
+app.post("/downvote/:questionID/:answerID", function(req, res){
+  let questionId = req.params.questionID;
+  let answerId = req.params.answerID;
+  db.updateDownvotes(questionId, answerId);
+  res.redirect("/view/" + questionId);
+});
+
+app.get("/remove/question/:id", function(req, res){
+  db.deleteQuestion(req.params.id);
+  res.redirect("/home");
+});
+
+app.get("/view/:questionID", function(req, res){
+  db.getQuestion(req.params.questionID, function(err, question){
+    if(err){
+      console.log(err);
+    }else{
+      console.log(question);
+      res.render("pages/view-question", {
+        question: question
+      })
+    }
+  })
+})
 
 let server = app.listen(port, function(req, res){
   console.log("Server started on port " + port);
---
+++ modules/database.js
@@ -0,0 +1,96 @@
+let mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/quoraDB");
+
+const answerSchema = mongoose.Schema({
+  answer: String,
+  url: String,
+  upvotes: Number,
+  downvotes: Number
+});
+
+const questionSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  imageURL: String,
+  answers: [answerSchema]
+});
+
+const Question = new mongoose.model("Question", questionSchema);
+
+exports.insertQuestion = function(object){
+  let question = new Question(object);
+  question.save();
+}
+
+exports.getAllQuestions = function(callback) {
+  Question.find(function(err, questions) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, questions);
+    }
+  });
+}
+
+exports.getQuestion = function(id, callback){
+  Question.findById(id, function(err, question) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, question);
+    }
+  });
+}
+
+exports.insertAnswer = function(id, answerText, answerImageURL){
+  let answer = {
+    answer: answerText,
+    url: answerImageURL,
+    upvotes: 0,
+    downvotes: 0
+  }
+  Question.findOne({_id: id}, function(err, result){
+    if(err){
+      console.log(err);
+    }else{
+      result.answers.push(answer);
+      result.save();
+    }
+  });
+}
+
+exports.updateUpvotes = function(questionId, answerId){
+  Question.findOne({_id: questionId}, function(err, result){
+    if(err){
+      console.log(err);
+    }else{
+      result.answers.forEach(function(answer){
+        if(String(answer._id) === String(answerId)){
+          answer.upvotes += 1;
+        }
+      });
+      result.save();
+    }
+  })
+}
+
+exports.updateDownvotes = function(questionId, answerId){
+  Question.findOne({_id: questionId}, function(err, result){
+    if(err){
+      console.log(err);
+    }else{
+      result.answers.forEach(function(answer){
+        if(String(answer._id) === String(answerId)){
+          answer.downvotes += 1;
+        }
+      });
+      result.save();
+    }
+  })
+}
+
+exports.deleteQuestion = function(questionId){
+  Question.findByIdAndDelete(questionId, function(err){
+    console.log(err);
+  });
+}
